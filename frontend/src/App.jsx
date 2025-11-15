3c64cb36cd9ba7fd37ece78a930d69ce94358ed4
Irina <iratushina17@gmail.com>
2025-11-15 12:53:08 +0000

    React project setup, all packages and app structure

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import { Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import Quiz from "./pages/Quiz";
+import Results from "./pages/Results";
+import History from "./pages/History";
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-[#03080B] text-white">
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/quiz" element={<Quiz />} />
+        <Route path="/results" element={<Results />} />
+        <Route path="/history" element={<History />} />
+      </Routes>
+    </div>
+  );
+}
